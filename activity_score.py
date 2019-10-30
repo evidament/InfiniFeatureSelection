fix: off-by-one error

activity_score.py
@@ -5,12 +5,12 @@ import sys
 from _writer import ffi, lib
 import _writer
 
-BUFFER_SIZE = 100000
+BUFFER_SIZE = 1000
 
 cffi_support.register_module(_writer)
 write = _writer.lib.write_data
 
-@jit((types.Array(types.float32, 2, "A"), types.Array(types.float32, 2, "A")), nopython=True, nogil=True, parallel=True, fastmath=True, cache=True)
+@jit((types.Array(types.float32, 2, "A"), types.Array(types.float32, 2, "A")), nopython=True, nogil=True, parallel=False, fastmath=False, cache=False)
 def activity_score(a, _):
     """Calculate activity score for column pairs
     
@@ -26,20 +26,21 @@ def activity_score(a, _):
 
     iqr = np.zeros(cols, dtype=np.float32)
     rank_arr = np.zeros((cols, rows), dtype=np.float32)
-    checkpoint = combinations_count//5
+    checkpoint = combinations_count//10
 
-    for index_base in prange(0, cols*cols//BUFFER_SIZE):
+    for index_base in range(0, cols*cols//BUFFER_SIZE+1):
         max_iqr = np.zeros(BUFFER_SIZE, dtype=np.float32)
         sp_coeff = np.zeros(BUFFER_SIZE, dtype=np.float32)
 
         for i in range(BUFFER_SIZE):
             index = index_base*BUFFER_SIZE + i
-
             j = index//cols
             k = index%cols
 
             if index%checkpoint==0:
                 percent = int(index/combinations_count*100)
+                if index>combinations_count:
+                    break
                 print(percent, "%")
 
             if k<=j:
@@ -48,9 +49,9 @@ def activity_score(a, _):
             col1 = a[:, j]
             col2 = a[:, k]
             if iqr[j]==0:
-                iqr[j] = np.quantile(col1, 0.75)-np.quantile(col1, 0.25)
+                iqr[j] = np.abs(np.quantile(col1, 0.75)-np.quantile(col1, 0.25))
             if iqr[k]==0:
-                iqr[k] = np.quantile(col2, 0.75)-np.quantile(col2, 0.25)
+                iqr[k] = np.abs(np.quantile(col2, 0.75)-np.quantile(col2, 0.25))
 
             if rank_arr[j][0]==0:
                 temp = col1.argsort()
@@ -66,6 +67,4 @@ def activity_score(a, _):
             max_iqr[i] = m_iqr
             sp_coeff[i] = 1-rho
 
-        res_iqr = ffi.from_buffer(max_iqr)
-        res_sp = ffi.from_buffer(sp_coeff)
-        write(res_iqr, res_sp, BUFFER_SIZE, float(index_base))
+        write(max_iqr.ctypes, sp_coeff.ctypes, BUFFER_SIZE, cols, float(index_base))